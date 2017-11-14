c4ab39f19286d1413fed80c1b4e781e092f253f6
seb9465 <cado.94@me.com>
2017-11-13 20:51:43 -0500

    SC : Adding third challenge.

---
+++ 3-Arithmetic_Operators/main.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(input())        
+    b = int(input())
+    
+    sum = a + b             # Sum of the two numbers.
+    dif = a - b             # The difference of the two numbers.
+    prod = a * b            # The product of the two numbers.
+    
+    print(sum)
+    print(dif)
+    print(prod)
