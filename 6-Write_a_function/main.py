6389151434064fcd98d0abd3bba4ae4ea99ba640
seb9465 <cado.94@me.com>
2017-11-13 21:06:47 -0500

    SC : Adding function challenge.

--- 6-Write_a_function/main.py
+++ 6-Write_a_function/main.py
@@ -2,8 +2,12 @@ def is_leap(year):
     leap = False
     
     # Write your logic here
-    if year % 4 is 0 and year % 100 is not 0 and year % 400 is 0:
-        leap = False
+    if year % 4 is 0:
+        leap = True
+        if year % 100 is 0:
+            leap = False
+            if year % 400 is 0:
+                leap = True
     
     return leap
 
